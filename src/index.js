33433cd162c6a4e38530a8c468c58db260f668ab
Ryan Wahle <ryanwahle8@gmail.com>
2023-01-05 18:01:20 -0600

    Worked on project/todo addition

--- src/index.js
+++ src/index.js
@@ -2,3 +2,42 @@ import './style.css';
 import appendGithub from './JSFiles/image.js'
 
 appendGithub()
+
+function createProject(projectName){
+    return{
+        name: projectName,
+        todos: [],
+    }
+}
+
+function appendProject(project){
+    projectList.push(project)
+}
+
+function createToDo(ToDoName, description, date, ){
+    return{
+        name: ToDoName,
+        desc: description,
+        dueDate: date,
+        priority: prio
+    }
+}
+
+function createProjectList(){
+    let defaultProject = createProject('Default Proj')
+
+    let projectList = [defaultProject]
+
+    return projectList
+}
+
+let projectList = createProjectList()
+
+let addProjectButton = document.querySelector('.project-add')
+
+addProjectButton.addEventListener('click', )
+
+
+
+
+
